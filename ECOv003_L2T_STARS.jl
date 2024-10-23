85ec1c51483cdc6872e2f204ee9f2780fe5f1c6b
Gregory H. Halverson <gregory.h.halverson@jpl.nasa.gov>
2024-10-23 11:33:34 -0700

    converting repository to Julia package

--- ECOv003_L2T_STARS.jl
+++ ECOv003_L2T_STARS.jl
@@ -1,16 +1,7 @@
-using YAML
+using ECOv003L2TSTARS
 
-# Check if a filename is provided as a command line argument
-if length(ARGS) < 1
-    println("Usage: julia ECOv003_L2T_STARS.jl <runconfig.yaml>")
-    exit(1)  # Exit with an error code
+function main(ARGS)
+    return ECOv003L2TSTARS.main(ARGS)
 end
 
-# Get the filename from the first command line argument
-runconfig_filename = ARGS[1]
-
-# Load the YAML file
-runconfig_dict = YAML.load_file(runconfig_filename)
-
-# Print the contents in YAML format
-println(YAML.write(runconfig_dict))
+exit(main(ARGS))
